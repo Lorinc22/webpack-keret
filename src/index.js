8a36cd04cd8d399dbd9dd5a45b09477e89b7d8b9
Krity Lorinc <krity.lorinc@gmail.com>
2022-09-29 15:12:01 +0200

    felkesz

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
+
+function ChangeKep()
+{
+    let url = document.getElementById('url').value;
+    document.getElementById('kep').src = url;
+    document.getElementById('width').defaultValue = document.getElementById('kep').width;
+}
+
+function width()
+{
+    let width = document.getElementById('width').value;
+    document.getElementById('kep').width = width;
+}
+
+function borderWidth()
+{
+    let border = document.getElementById('border').value;
+    document.getElementById('kep').style.border = border + 'px solid black' ;
+}
+function link()
+{
+    let link = document.getElementById('link').value;
+    document.getElementById('kep').src = url;
+    document.getElementById('width').defaultValue = document.getElementById('kep.').width;
+}
+
+function macska(){
+    let macska = document.getElementById('macska').value;
+    document.getElementById('kep').src =url;
+    document.getElementById('widht').defaultValue=document.getAnimations('kep').width
+    
+
+}
 
